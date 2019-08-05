81a032513f959c729781bdd9c9dcdd6675cb508e
Iim Nur Diansyah <alterra@alterras-MacBook-Air-2.local>
2019-08-05 15:48:10 +0700

    first

---
+++ go-admin.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	_ "github.com/chenhg5/go-admin/adapter/beego"
+	"github.com/chenhg5/go-admin/engine"
+	"github.com/chenhg5/go-admin/examples/datamodel"
+	"github.com/chenhg5/go-admin/modules/config"
+	"github.com/chenhg5/go-admin/modules/db"
+	"github.com/chenhg5/go-admin/plugins/admin"
+	"github.com/chenhg5/go-admin/plugins/example"
+	"github.com/chenhg5/go-admin/template/types"
+)
+
+func main() {
+	app := beego.NewApp()
+
+	eng := engine.Default()
+
+	cfg := config.Config{
+		DATABASE: []config.Database{
+			{
+				HOST:         "127.0.0.1",
+				PORT:         "3306",
+				USER:         "root",
+				PWD:          "",
+				NAME:         "godmin",
+				MAX_IDLE_CON: 50,
+				MAX_OPEN_CON: 150,
+				DRIVER:       db.DriverMysql,
+			},
+		},
+		DOMAIN: "localhost",
+		PREFIX: "admin",
+		INDEX:  "/",
+		DEBUG:  true,
+	}
+
+	// you can custom a plugin like:
+
+	examplePlugin := example.NewExample()
+
+	if err := eng.AddConfig(cfg).AddPlugins(admin.NewAdmin(datamodel.Generators), examplePlugin).
+		Use(app); err != nil {
+		panic(err)
+	}
+
+	// you can custom your pages like:
+
+	app.Handlers.Get("/"+cfg.PREFIX+"/custom", func(ctx *context.Context) {
+		engine.Content(ctx, func() types.Panel {
+			return datamodel.GetContent()
+		})
+	})
+
+	beego.BConfig.Listen.HTTPAddr = "127.0.0.1"
+	beego.BConfig.Listen.HTTPPort = 9087
+	app.Run()
+}
